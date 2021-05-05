06d7b3806dc61735857f031ab0d67fb5cecca9cf
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-05-05 17:36:27 +0900

    [Add]sprintf-書式を揃える

---
+++ template_file/sample12.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+    $date = sprintf('%04d年 %02d月 %02d日', 2021, 5 ,5); // 2020年05月05日 d→diget(数字) s→string(文字) 04→4桁で揃える
+?>
+</pre>
+</main>
+</body>    
+</html>
